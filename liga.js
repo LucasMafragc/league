86c07a0c484de06b21d689cb0281788cb2a6ebdc
LucasMafragc <131695680+LucasMafragc@users.noreply.github.com>
2023-05-15 11:12:51 -0300

    atualização do nav para js

---
+++ liga.js
@@ -0,0 +1,8 @@
+function clickherois() {
+    var navherois = document.querySelector('.subherois').style.display;
+    if(navherois=='none'){
+        document.querySelector('.subherois').style.display = 'block';
+        } 
+    else {document.querySelector('.subherois').style.display= 'none';}
+}
+
