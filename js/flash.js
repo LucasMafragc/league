bbd457ad56b1b38fa6b15b05ab2da456367734d7
LucasMafragc <131695680+LucasMafragc@users.noreply.github.com>
2023-05-22 22:41:46 -0300

    atualização do flash tela pequena

--- js/flash.js
+++ js/flash.js
@@ -1,6 +1,9 @@
 const menuoculto = document.querySelector('.hamburguer')
 const menuinicial = document.querySelector('.submenu')
 
+const iconeBackground = document.querySelector('.iconeBackground')
+const textoBackground = document.querySelector('.textoBackground')
+
 function clicar(){
     if(window.getComputedStyle(menuinicial).display ==='none'){
         menuinicial.style.display = 'block'
@@ -14,4 +17,18 @@ function fecharmenu(){
     if( window.getComputedStyle(menuinicial).display =='block'){
        menuinicial.style.display = 'none'
     }
+}
+
+function ativarBackground(){
+    if( iconeBackground.innerHTML == 'toggle_off'){
+        iconeBackground.innerHTML = 'toggle_on'
+        textoBackground.innerHTML = 'Para desligar a imagem de fundo click no botão abaixo'
+        document.body.style.backgroundImage = "url('https://e1.pxfuel.com/desktop-wallpaper/483/645/desktop-wallpaper-the-flash-by-an0rak18-iphone-flash-comic.jpg')"
+
+    } else {
+        iconeBackground.innerHTML = 'toggle_off'
+        textoBackground.innerHTML = 'Para ligar a imagem de fundo click no botão abaixo'
+        document.body.style.background = ""
+    }
+    
 }
