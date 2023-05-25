04df268f2a6416b18d97fd88d550239cfca0998b
LucasMafragc <131695680+LucasMafragc@users.noreply.github.com>
2023-05-25 18:37:06 -0300

    pagina principal atualizada

--- liga.js
+++
@@ -1,8 +0,0 @@
-function clickherois() {
-    var navherois = document.querySelector('.subherois').style.display;
-    if(navherois=='none'){
-        document.querySelector('.subherois').style.display = 'block';
-        } 
-    else {document.querySelector('.subherois').style.display= 'none';}
-}
-
