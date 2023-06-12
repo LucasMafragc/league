08d5de289afb69d261d0ef9d712cf4b80087a5f0
LucasMafragc <131695680+LucasMafragc@users.noreply.github.com>
2023-06-12 16:22:11 -0300

    atualização mulher maravilha

---
+++ js/mulherMaravilha.js
@@ -0,0 +1,60 @@
+const menuoculto = document.querySelector('.hamburguer')
+const menuinicial = document.querySelector('.submenu')
+
+const iconeBackground = document.querySelector('.iconeBackground')
+const textoBackground = document.querySelector('.textoBackground')
+
+function clicar(){
+    if(innerWidth <600){
+        if(window.getComputedStyle(menuinicial).display ==='none'){
+        menuinicial.style.display = 'block'
+    }else{
+        menuinicial.style.display = 'none'
+    }
+    }
+}
+
+function fecharmenu(){
+    if (innerWidth<600){
+        if( window.getComputedStyle(menuinicial).display =='block'){
+            menuinicial.style.display = 'none'
+         }
+    }
+    
+    
+}
+
+function ativarBackground(){
+    if(innerWidth <600){
+        if( iconeBackground.innerHTML == 'toggle_off'){
+            iconeBackground.innerHTML = 'toggle_on'
+            textoBackground.innerHTML = 'Para desligar a imagem de fundo click no botão abaixo'
+            document.body.style.backgroundImage = "url('https://images4.alphacoders.com/853/853715.png')"
+    
+        } else {
+            iconeBackground.innerHTML = 'toggle_off'
+            textoBackground.innerHTML = 'Para ligar a imagem de fundo click no botão abaixo'
+            document.body.style.background = ""
+        }
+    }else{
+        if( iconeBackground.innerHTML == 'toggle_off'){
+            iconeBackground.innerHTML = 'toggle_on'
+            textoBackground.innerHTML = 'Para desligar a imagem de fundo click no botão abaixo'
+            document.body.style.backgroundImage = "url('https://images4.alphacoders.com/853/853715.png')"
+    
+        } else {
+            iconeBackground.innerHTML = 'toggle_off'
+            textoBackground.innerHTML = 'Para ligar a imagem de fundo click no botão abaixo'
+            document.body.style.background = ""
+        }
+    }
+}
+
+function monitorar(){
+    if(innerWidth > 600){
+        menuinicial.style.display = 'block';
+    } else{
+        menuinicial.style.display = 'none';
+        
+    }
+}
